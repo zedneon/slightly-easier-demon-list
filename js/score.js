4cf45477953c0f98f36db53573d3cb33457796ad
nickneon <114791412+zedneon@users.noreply.github.com>
2026-03-22 12:43:54 -0700

    Update score.js

--- js/score.js
+++ js/score.js
@@ -11,10 +11,10 @@ const scale = 1;
  * @returns {Number}
  */
 export function score(rank, percent, minPercent) {
-    if (rank > 100) {
+    if (rank > 150) {
         return 0;
     }
-    if (rank > 50 && percent < 100) {
+    if (rank > 75 && percent < 100) {
         return 0;
     }
 
