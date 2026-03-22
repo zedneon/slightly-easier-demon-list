71a900e4e86fefd6737a37a431c3ce4e8e8701a2
nickneon <114791412+zedneon@users.noreply.github.com>
2026-03-22 15:14:03 -0700

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -56,7 +56,7 @@ export default {
                         </li>
                     </ul>
                     <h2>Records</h2>
-                    <p v-if="selected + 1 <= 50"><strong>{{ level.percentToQualify }}%</strong> or better to qualify</p>
+                    <p v-if="selected + 1 <= 75"><strong>{{ level.percentToQualify }}%</strong> or better to qualify</p>
                     <p v-else-if="selected +1 <= 150"><strong>100%</strong> or better to qualify</p>
                     <p v-else>This level does not accept new records.</p>
                     <table class="records">
